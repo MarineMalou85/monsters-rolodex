a12261f46acdf10c622a75a013d4098037015028
Marine <marinegouliat@gmail.com>
2020-02-16 19:06:51 +0100

    Updates

---
+++ precache-manifest.a147d3481b4678fb3364061079a71a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b451cad8637fec7efdf7c6ff96e4aa38",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "30a0a57caddc8afbcb86",
+    "url": "/monsters-rolodex/static/css/main.00e6aaee.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30a0a57caddc8afbcb86",
+    "url": "/monsters-rolodex/static/js/main.900fae05.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
